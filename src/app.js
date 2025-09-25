6ab8cbd0b60ae4e8d450a2ea0d689af3ae3435ba
Jonathan Palma <166739509+gitttjonzzz@users.noreply.github.com>
2025-09-25 00:23:26 +0000

    soccer generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".btn").addEventListener("click",generateSoccer) 
 };
+const generateSoccer= ()=>{
+  let player =["Ronaldo","Messi","Mbappe","Foden","Lamine Yamal"];
+  let stadium =["Anfield","Wembley","Bernabeu","Camp Nou","Old Trafford"];
+  let team=["Real Madrid","Barcelona","Liverpool","Arsenal","Inter Miami"];
+  let league =["Serie A","Premier league","La Liga","MLS","Bundesliga"];
+  let position =["Striker","Left Wing","Center Back","Right Wing","Midfielder"];
+
+
+  let playerChoice=Math.floor(Math.random()*player.length);
+  let stadiumChoice =Math.floor(Math.random()*stadium.length);
+  let teamChoice=Math.floor(Math.random()*team.length);
+  let leagueChoice=Math.floor(Math.random()*league.length);
+  let positionChoice =Math.floor(Math.random()*position.length);
+
+  let playerDiv =document.getElementById('player');
+  let stadiumDiv =document.getElementById('stadium');
+  let teamDiv =document.getElementById('team');
+  let leagueDiv =document.getElementById('league');
+  let positionDiv =document.getElementById('position');
+
+  
+  playerDiv.innerHTML = player[playerChoice];
+  stadiumDiv.innerHTML = stadium[stadiumChoice];
+  teamDiv.innerHTML = team[teamChoice];
+  leagueDiv.innerHTML = league[leagueChoice];
+  positionDiv.innerHTML = position[positionChoice];
+}
